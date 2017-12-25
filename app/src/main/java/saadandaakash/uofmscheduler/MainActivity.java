b337a5dcdd90fb17c2d54821c564f2521e5d991a
Saad <saadmallick98@gmail.com>
2017-12-25 17:39:14 -0500

    Font and Dropdown selection update

--- app/src/main/java/saadandaakash/uofmscheduler/MainActivity.java
+++ app/src/main/java/saadandaakash/uofmscheduler/MainActivity.java
@@ -1,12 +1,8 @@
 package saadandaakash.uofmscheduler;
 
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -16,6 +12,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
+
+import saadandaakash.uofmscheduler.Fragments.SelectionFragment;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
